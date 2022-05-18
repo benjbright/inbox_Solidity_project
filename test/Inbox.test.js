47da9c1332fc19b171230bf712583fce4c5ef8fa
Ben <105577567+benjbright@users.noreply.github.com>
2022-05-18 20:33:39 +0100

    Course work update 18/05/22

--- test/Inbox.test.js
+++ test/Inbox.test.js
@@ -1,1 +1,5 @@
-// contract test code will go here
+const assert = require("assert")
+const ganache = require("ganache-cli")
+const Web3 = require("web3") // note capitalisation for the constructor function
+const web3 = new Web3(ganache.provider())
+// Create new instance of Web3 and connect to locally hosted ganache test network
