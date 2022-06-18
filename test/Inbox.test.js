43a71addbe0debbc3229808bf80fd4fdfa575487
Ben <105577567+benjbright@users.noreply.github.com>
2022-06-18 17:25:22 +0100

    Update Inbox.test.js

--- test/Inbox.test.js
+++ test/Inbox.test.js
@@ -4,19 +4,32 @@ const Web3 = require("web3") // note capitalisation for the constructor function
 const web3 = new Web3(ganache.provider())
 // Create new instance of Web3 and connect to locally hosted ganache test network
 // Can only deploy a contract when you have access to an account
-beforeEach(() => {
+const { interface, bytecode } = require("../compile")
+
+let accounts
+let inbox
+
+beforeEach(async () => {
   // Get a list of all accounts
   // Note using the instance of Web3 - 'web3'
   // Almost every web3 function is async and returns a promise
-  web3.eth.getAccounts().then((fetchedAccounts) => {
-    console.log(fetchedAccounts)
-  })
+  accounts = await web3.eth.getAccounts()
 
   // Use one of those accounts to deploy the contract
+  inbox = await new web3.eth.Contract(JSON.parse(interface))
+    .deploy({
+      data: bytecode,
+      arguments: ["Hi there!"],
+    })
+    .send({ from: accounts[0], gas: "1000000" })
 })
 
 describe("Inbox", () => {
-  it("deploys a contract", () => {})
+  it("deploys a contract", () => {
+    // ok method - checks whatever is passed in to the assert
+    // function actually exists
+    assert.ok(inbox.options.address)
+  })
 })
 
 // class Car {
