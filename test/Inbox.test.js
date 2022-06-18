47ad14d1b19578f0f5114edc0327cafe984d4360
Ben <105577567+benjbright@users.noreply.github.com>
2022-06-18 11:11:37 +0100

    Update Inbox.test.js

--- test/Inbox.test.js
+++ test/Inbox.test.js
@@ -3,16 +3,31 @@ const ganache = require("ganache-cli")
 const Web3 = require("web3") // note capitalisation for the constructor function
 const web3 = new Web3(ganache.provider())
 // Create new instance of Web3 and connect to locally hosted ganache test network
+// Can only deploy a contract when you have access to an account
+beforeEach(() => {
+  // Get a list of all accounts
+  // Note using the instance of Web3 - 'web3'
+  // Almost every web3 function is async and returns a promise
+  web3.eth.getAccounts().then((fetchedAccounts) => {
+    console.log(fetchedAccounts)
+  })
 
-class Car {
-  park() {
-    return "stopped"
-  }
+  // Use one of those accounts to deploy the contract
+})
 
-  drive() {
-    return "vroom"
-  }
-}
+describe("Inbox", () => {
+  it("deploys a contract", () => {})
+})
+
+// class Car {
+//   park() {
+//     return "stopped"
+//   }
+
+//   drive() {
+//     return "vroom"
+//   }
+// }
 
 // Mocha functions
 // it - run a test and make an assertion
